240f198abcdbf02e289a334545159e3b67f6799b
HangZhangZ <101213267+HangZhangZ@users.noreply.github.com>
2024-09-03 15:34:10 +0200

    Update SAM_DataProcess.py

--- SAM_DataProcess.py
+++ SAM_DataProcess.py
@@ -161,11 +161,58 @@ if __name__ == "__main__":
         print("Initialize SAM-HQ Predictor")
         predictor = SamAutomaticMaskGenerator(build_sam_hq(checkpoint=sam_hq_checkpoint).to(device))
     else:
-        predictor = SamAutomaticMaskGenerator(model=build_sam(checkpoint=sam_checkpoint).to(device),
-                                              points_per_side=16, # 32
-                                              min_mask_region_area=300, # None
-                                              pred_iou_thresh=0.95 # 0.88
-                                              ) 
+        predictor = SamAutomaticMaskGenerator(
+            model=build_sam(checkpoint=sam_checkpoint).to(device),
+            points_per_side=16, # 32
+            min_mask_region_area=500, # None
+            pred_iou_thresh=0.90, # 0.88
+            stability_score_thresh= 0.95, #
+            stability_score_offset = 1.0, #
+            box_nms_thresh = 0.5, #0.7
+            crop_n_layers = 0, #
+            crop_nms_thresh = 0.7, #
+            crop_overlap_ratio = 512 / 1500, #
+            crop_n_points_downscale_factor = 1) 
+
+        """
+          model (Sam): The SAM model to use for mask prediction.
+          points_per_side (int or None): The number of points to be sampled
+            along one side of the image. The total number of points is
+            points_per_side**2. If None, 'point_grids' must provide explicit
+            point sampling.
+          points_per_batch (int): Sets the number of points run simultaneously
+            by the model. Higher numbers may be faster but use more GPU memory.
+          pred_iou_thresh (float): A filtering threshold in [0,1], using the
+            model's predicted mask quality.
+          stability_score_thresh (float): A filtering threshold in [0,1], using
+            the stability of the mask under changes to the cutoff used to binarize
+            the model's mask predictions.
+          stability_score_offset (float): The amount to shift the cutoff when
+            calculated the stability score.
+          box_nms_thresh (float): The box IoU cutoff used by non-maximal
+            suppression to filter duplicate masks.
+          crop_n_layers (int): If >0, mask prediction will be run again on
+            crops of the image. Sets the number of layers to run, where each
+            layer has 2**i_layer number of image crops.
+          crop_nms_thresh (float): The box IoU cutoff used by non-maximal
+            suppression to filter duplicate masks between different crops.
+          crop_overlap_ratio (float): Sets the degree to which crops overlap.
+            In the first crop layer, crops will overlap by this fraction of
+            the image length. Later layers with more crops scale down this overlap.
+          crop_n_points_downscale_factor (int): The number of points-per-side
+            sampled in layer n is scaled down by crop_n_points_downscale_factor**n.
+          point_grids (list(np.ndarray) or None): A list over explicit grids
+            of points used for sampling, normalized to [0,1]. The nth grid in the
+            list is used in the nth crop layer. Exclusive with points_per_side.
+          min_mask_region_area (int): If >0, postprocessing will be applied
+            to remove disconnected regions and holes in masks with area smaller
+            than min_mask_region_area. Requires opencv.
+          output_mode (str): The form masks are returned in. Can be 'binary_mask',
+            'uncompressed_rle', or 'coco_rle'. 'coco_rle' requires pycocotools.
+            For large resolutions, 'binary_mask' may consume large amounts of
+            memory.
+        """
+
 
     # build loop
     image_paths = glob.glob('image_dataset' + '/*.jpg')
