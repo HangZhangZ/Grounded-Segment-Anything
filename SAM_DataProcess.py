1109fe691ccd60ba8ab82b8a59c35084f21649b1
HangZhangZ <101213267+HangZhangZ@users.noreply.github.com>
2024-09-03 21:05:31 +0200

    Update SAM_DataProcess.py

--- SAM_DataProcess.py
+++ SAM_DataProcess.py
@@ -91,7 +91,8 @@ def parse_mask_region(img, output_dir, masks_all, id):
         # init general canvas
         mask_img = np.zeros(mask_np.shape)
         mask_img[mask_np == True] = 255
-        mask_img_all[mask_np == True] = seg_colors[idx]
+        # mask_img_all[mask_np == True] = seg_colors[idx]
+        mask_img_all[mask_np == True] += seg_colors[idx]/4
         img_filtered = img.copy()
         img_filtered[mask_np == False, :] = 0
         # save mask region
