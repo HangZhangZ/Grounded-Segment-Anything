c0943bce4ecab82b3901b7eb6d97ebe9661ba863
HangZhangZ <101213267+HangZhangZ@users.noreply.github.com>
2024-09-03 16:16:52 +0200

    Update SAM_DataProcess.py

--- SAM_DataProcess.py
+++ SAM_DataProcess.py
@@ -172,10 +172,10 @@ if __name__ == "__main__":
             stability_score_thresh= 0.93, #0.95
             stability_score_offset = 1.0, #
             box_nms_thresh = 0.7, #0.7
-            crop_n_layers = 1, #0
+            crop_n_layers = 0, #0
             crop_nms_thresh = 0.7, #0.7
             crop_overlap_ratio = 512 / 1500, #
-            crop_n_points_downscale_factor = 2 #1
+            crop_n_points_downscale_factor = 1 #1
             ) 
 
         """
