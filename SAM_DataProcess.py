db9b5460e8f61a86936ca93deb08ccf5a23b335d
HangZhangZ <101213267+HangZhangZ@users.noreply.github.com>
2024-09-03 21:18:23 +0200

    Update SAM_DataProcess.py

--- SAM_DataProcess.py
+++ SAM_DataProcess.py
@@ -168,7 +168,7 @@ if __name__ == "__main__":
             model=build_sam(checkpoint=sam_checkpoint).to(device),
             points_per_side=int(max_mask/2), # 32
             min_mask_region_area=100000, # None
-            pred_iou_thresh=0.8, # 0.88
+            pred_iou_thresh=0.95, # 0.88
             stability_score_thresh= 0.95, #0.95
             stability_score_offset = 1.0, #
             box_nms_thresh = 0.9, #0.7
